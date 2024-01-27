a3632a9ad958d6cd0ebe0c9f31a9d1ba8de0cc79
Jonathan Duck <duckbrain30@gmail.com>
2024-01-27 11:20:10 -0800

    Remove unused options.js

--- js/options.js
+++
@@ -1,9 +0,0 @@
-function optionSet(o) {
-	SetOption(o.srcElement.id, o.srcElement.checked);
-}
-
-document.onload = function() {
-document.getElementById('noEyes').onclick = optionSet;
-document.getElementById('noOutline').onclick = optionSet;
-document.getElementById('black') = optionSet;
-}
